7631008b8bf420545bb3c77d19f8062ca21ddf14
Halima KOUNDI <hkoundi@gmail.com>
2016-10-24 16:39:59 +0100

    cleaning up tests

--- c#/TripServiceKata/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/TripServiceKata/TripServiceKata.Tests/TripServiceShould.cs
@@ -13,15 +13,8 @@ namespace TripServiceKata.Tests
         private UserSessionTest _userSession;
         private TripService _tripService;
         private TripDAO _tripDao;
-        private User.User _loggedInUser;
-        private User.User _friendedUser;
-
-        public TripServiceShould()
-        {
-            _loggedInUser = new User.User();
-            _friendedUser = new User.User();
-        }
-
+        private readonly User.User _loggedInUser = new User.User();
+        private readonly User.User _friendedUser = new User.User();
 
         [SetUp]
         public void SetUp()
@@ -29,27 +22,23 @@ namespace TripServiceKata.Tests
             _userSession = Substitute.For<UserSessionTest>();
             _tripDao = Substitute.For<TripDAO>();
             _tripService = new TripService(_userSession, _tripDao);
-
         }
 
         [Test]
         public void throw_an_error_when_user_not_logged_in()
         {
-            var user = new User.User();
-
             Assert.Throws<UserNotLoggedInException>(
                 () =>
                 {
-                    _tripService.GetTripsByUser(user);
+                    _tripService.GetTripsByUser(new User.User());
                 });
         }
 
         [Test]
         public void return_no_trip_when_user_has_no_friends()
         {
-            var user = new User.User();
-            _userSession.IsUserLoggedIn(user).Returns<bool>(x => true);
-            _userSession.GetLoggedUser().Returns(x => user);
+            _userSession.IsUserLoggedIn(_loggedInUser).Returns<bool>(x => true);
+            _userSession.GetLoggedUser().Returns(x => _loggedInUser);
             var notFriendedUser = new User.User();
 
             var trips = _tripService.GetTripsByUser(notFriendedUser);
@@ -60,17 +49,23 @@ namespace TripServiceKata.Tests
         [Test]
         public void return_trips_of_a_friend_user()
         {
-            var trip = new Trip.Trip();
-            _friendedUser.AddTrip(trip);
-            _friendedUser.AddFriend(_loggedInUser);
-            _userSession.IsUserLoggedIn(_loggedInUser).Returns<bool>(x => true);
-            _userSession.GetLoggedUser().Returns(x => _loggedInUser);
-            var expectedTrips = new List<Trip.Trip> {trip};
-            _tripDao.FindTripsByUser(_friendedUser).Returns(x => expectedTrips);
+            var expectedTrips = GivenUserHasFriendsWithTrips();
 
             var trips = _tripService.GetTripsByUser(_friendedUser);
 
             Assert.That(trips, Is.EqualTo(expectedTrips));
+        }
+
+        private List<Trip.Trip> GivenUserHasFriendsWithTrips()
+        {
+            var toParis = new Trip.Trip();
+            _friendedUser.AddTrip(toParis);
+            _friendedUser.AddFriend(_loggedInUser);
+            _userSession.IsUserLoggedIn(_loggedInUser).Returns<bool>(x => true);
+            _userSession.GetLoggedUser().Returns(x => _loggedInUser);
+            var expectedTrips = new List<Trip.Trip> {toParis};
+            _tripDao.FindTripsByUser(_friendedUser).Returns(x => expectedTrips);
+            return expectedTrips;
         }
     }
 
