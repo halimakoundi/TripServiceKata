210a857894bfe4b54d163b633b6caf0421d3af44
Halima KOUNDI <hkoundi@gmail.com>
2016-10-24 14:44:37 +0100

    trip service throws an exception when user not logged in

---
+++ c#/TripServiceKata/TripServiceKata.Tests/TripServiceShould.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using TripServiceKata.Exception;
+using TripServiceKata.Trip;
+
+namespace TripServiceKata.Tests
+{
+    [TestFixture]
+    public class TripServiceShould
+    {
+
+        [Test]
+        public void throw_an_error_when_user_not_logged_in()
+        {
+            var user = new User.User();
+            var tripService = new TripService();
+
+
+            Assert.Throws<DependendClassCallDuringUnitTestException>(
+                () =>
+                {
+                    var trips = tripService.GetTripsByUser(user);
+                });
+        }
+    }
+}
